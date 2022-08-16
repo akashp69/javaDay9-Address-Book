239ced6249595c4e82587387b2b5f6eb544ecb44
akashp69 <akashpakhare499@gmail.com>
2022-08-16 21:38:38 +0530

    Ability to multiple Person to Address Book

--- src/javaday9addressbook/AddressBookMain.java
+++ src/javaday9addressbook/AddressBookMain.java
@@ -9,47 +9,56 @@ public class AddressBookMain {
         String firstName, lastName, address, city, state, email;
         int zip;
         long phoneNo;
-        Scanner sc = new Scanner(System.in);
-        AddressBook contact = new AddressBook();
-        System.out.print("Enter First Name  : ");
-        firstName = sc.nextLine();
-        contact.setFirstName(firstName);
-        System.out.print("\nEnter Last Name  : ");
-        lastName = sc.nextLine();
-        contact.setLastName(lastName);
-        System.out.print("\nEnter E-mail  : ");
-        email = sc.nextLine();
-        contact.setEmail(email);
-        System.out.print("\nEnter Address  : ");
-        address = sc.nextLine();
-        contact.setAddress(address);
-        System.out.print("\nEnter City  : ");
-        city = sc.nextLine();
-        contact.setCity(city);
-        System.out.print("\nEnter State  : ");
-        state = sc.nextLine();
-        contact.setState(state);
-        System.out.print("\nEnter Phone Number  : ");
-        phoneNo = sc.nextLong();
-        contact.setPhoneNo(phoneNo);
-        System.out.print("\nEnter Zip  : ");
-        zip = sc.nextInt();
-        contact.setZip(zip);
+        char option;
+        ArrayList<AddressBook> contactArr = new ArrayList<AddressBook>();
 
-        Console console = new Console();
-        console.showContact(contact);
+        Scanner sc = new Scanner(System.in);
+        do {
+            AddressBook contact = new AddressBook();
+            System.out.print("Enter First Name  : ");
+            firstName = sc.nextLine();
+            contact.setFirstName(firstName);
+            System.out.print("\nEnter Last Name  : ");
+            lastName = sc.nextLine();
+            contact.setLastName(lastName);
+            System.out.print("\nEnter E-mail  : ");
+            email = sc.nextLine();
+            contact.setEmail(email);
+            System.out.print("\nEnter Address  : ");
+            address = sc.nextLine();
+            contact.setAddress(address);
+            System.out.print("\nEnter City  : ");
+            city = sc.nextLine();
+            contact.setCity(city);
+            System.out.print("\nEnter State  : ");
+            state = sc.nextLine();
+            contact.setState(state);
+            System.out.print("\nEnter Phone Number  : ");
+            phoneNo = sc.nextLong();
+            contact.setPhoneNo(phoneNo);
+            System.out.print("\nEnter Zip  : ");
+            zip = sc.nextInt();
+            contact.setZip(zip);
+            contactArr.add(contact);
+            System.out.print(
+                    "\nDo you want to add one more contact? press Y / N : ");
+            option = sc.next().charAt(0);
+            sc.nextLine();
+        } while (option == 'Y');
+
+        Console  console = new Console ();
         System.out.print("\nDo you want to edit? press Y / N : ");
         char editOption = sc.next().charAt(0);
         if (editOption == 'Y') {
-            console.editContact(contact);
+            console.editContact(contactArr);
         }
-        console.showContact(contact);
+        console.showContact(contactArr);
         System.out.print("\nDo you want to delete? press Y / N : ");
         char deleteOption = sc.next().charAt(0);
         if (deleteOption == 'Y') {
-            contact = console.deleteContact(contact);
+            contactArr = console.deleteContact(contactArr);
         }
-        console.showContact(contact);
+        console.showContact(contactArr);
         sc.close();
     }
 }
